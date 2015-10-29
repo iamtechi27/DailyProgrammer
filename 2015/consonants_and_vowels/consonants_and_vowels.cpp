81665e88a7d57c2d97f1bc32fe70183b8922f5d4
techi <iamtechi27@gmail.com>
2015-10-28 20:59:16 -0700

    'Consonants and Vowels' updated Now accepts uppercase or lowercase input, output corresponds.

--- 2015/consonants_and_vowels/consonants_and_vowels.cpp
+++ 2015/consonants_and_vowels/consonants_and_vowels.cpp
@@ -5,14 +5,24 @@
 using namespace std;
 
 string cons = "bcdfghjklmnpqrstvwxyz";
+string CONS = "BCDFGHJKLMNPQRSTVWXYZ";
 string vows = "aeiou";
+string VOWS = "AEIOU";
 
 void addConsonant(char c) {
-	cout << cons.at(rand()%21);
+	if (c == 'c') {
+		cout << cons.at(rand()%21);
+	} else {
+		cout << CONS.at(rand()%21);
+	}
 }
 
 void addVowel(char v) {
-	cout << vows.at(rand()%5);
+	if (v == 'v') {
+		cout << vows.at(rand()%5);
+	} else {
+		cout << VOWS.at(rand()%5);
+	}
 }
 
 int main() {
@@ -20,7 +30,7 @@ int main() {
 	srand(time(NULL));
 	getline(cin, input);
 	for (int i = 0; i < input.length(); i++) {
-		if (input.at(i) == 'c') {
+		if (input.at(i) == 'c' || input.at(i) == 'C') {
 			addConsonant(input.at(i));
 		} else {
 			addVowel(input.at(i));
